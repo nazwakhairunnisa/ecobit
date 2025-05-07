79e6c4feddf62bbc8b1aa74b8a0663d34597589d
FirdauszyMelati <firdauszymelatisyahputri07.com>
2025-05-07 13:38:27 +0700

    add page thankyou

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,28 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/thankyou', function () {
+    return view('thankyou');
+});
+
+Route::get('/faq', function () {
+    $faqs = [
+        [
+            'question' => 'inter per fermentum eget ?',
+            'answer' => 'Integer tempus augue id enim tristique, et dapibus sapien ultricies. Quisque facilisis enim nec dui rhoncus',
+        ],
+        [
+            'question' => 'inter per fermentum eget ?',
+            'answer' => 'Integer tempus augue id enim tristique, et dapibus sapien ultricies. Quisque facilisis enim nec dui rhoncus',
+        ],
+        [
+            'question' => 'inter per fermentum eget ?',
+            'answer' => 'Integer tempus augue id enim tristique, et dapibus sapien ultricies. Quisque facilisis enim nec dui rhoncus',
+        ],
+    ];
+    return view('faq', compact('faqs'));
+});
+
+
+
 require __DIR__.'/auth.php';
