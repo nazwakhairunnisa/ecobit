897cbcc6c2460b26372f2d797c0236b5da9e4427
nazwa khairunnisa <nazwaamanalu@gmail.com>
2025-05-09 11:58:34 +0700

    add feature section

--- resources/views/components/features.blade.php
+++ resources/views/components/features.blade.php
@@ -1,3 +1,21 @@
 <section id="features">
-    <!-- ini untuk features page -->
+    <div class="flex w-full flex-col">
+        <div class="py-4 px-7 bg-[#172e45] text-white rounded-2xl inline-block justify-center items-center flex mx-auto">
+            <h1 class="text-5xl font-bold">ABOUT FEATURES</h1>
+        </div>
+        <div class="mt-36 flex flex-col lg:flex-row justify-between items-center px-8 lg:px-20 overflow-visible">
+            <div class="card bg-base-100 w-96 shadow-sm border border-gray-300 overflow-visible">
+                <div class="card-body items-center h-20">
+                    <img
+                    src="{{ asset('assets/img/feature1.png') }}"
+                    alt="feature1"
+                    class="rounded-xl w-60 z-30 -mt-36" />
+                </div>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
