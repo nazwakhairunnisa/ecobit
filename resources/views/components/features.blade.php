e70abd3a17f2b8b08120bca9fea0d471ccb9d40a
nazwa khairunnisa <nazwaamanalu@gmail.com>
2025-05-11 10:59:32 +0700

    add dashboard blade

--- resources/views/components/features.blade.php
+++ resources/views/components/features.blade.php
@@ -1,14 +1,26 @@
 <section id="features">
-    <div class="flex w-full flex-col">
+    <div class="flex w-full flex-col mt-12">
         <div class="py-4 px-7 bg-[#172e45] text-white rounded-2xl inline-block justify-center items-center flex mx-auto">
             <h1 class="text-5xl font-bold">ABOUT FEATURES</h1>
         </div>
-        <div class="mt-36 flex flex-col lg:flex-row justify-between items-center px-8 lg:px-20 overflow-visible">
+        <div class="mt-36 flex flex-col lg:flex-row justify-center items-center px-4 lg:px-8 py-4 gap-36 overflow-visible">
             <div class="card bg-base-100 w-96 shadow-sm border border-gray-300 overflow-visible">
                 <div class="card-body items-center h-20">
                     <img
                     src="{{ asset('assets/img/feature1.png') }}"
                     alt="feature1"
+                    class="rounded-xl w-60 z-30 -mt-36" />
+                </div>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                </div>
+            </div>
+            <div class="card bg-base-100 w-96 shadow-sm border border-gray-300 overflow-visible">
+                <div class="card-body items-center h-20">
+                    <img
+                    src="{{ asset('assets/img/feature2.png') }}"
+                    alt="feature2"
                     class="rounded-xl w-60 z-30 -mt-36" />
                 </div>
                 <div class="card-body items-center text-center">
