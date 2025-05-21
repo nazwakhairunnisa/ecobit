9e598bb8388d37d21d2f41ff1a3df4b0689f0625
nazwa khairunnisa <nazwaamanalu@gmail.com>
2025-05-21 08:50:42 +0700

    benerin edit profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,7 @@
-<x-app-layout>
-    <x-slot name="header">
+@extends('layouts.app')
+
+@section('content')
+<x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
@@ -64,4 +66,4 @@
         }
     });
 </script>
-</x-app-layout>
+@endsection
